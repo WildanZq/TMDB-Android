fe6fbc3c6062982ad88d319efcfa5630d70ce633
WildanZq <wildan2wildan@gmail.com>
2017-06-13 09:31:08 +0700

    init

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl230/stdmovie/model/Saved.java
@@ -0,0 +1,34 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl230.stdmovie.model;
+
+/**
+ * Created by wildan on 14/05/2017.
+ */
+
+public class Saved {
+    String title;
+    String desc;
+
+    public Saved() {
+    }
+
+    public Saved(String title, String desc) {
+        this.title = title;
+        this.desc = desc;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
